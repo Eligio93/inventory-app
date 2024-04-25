8e3473d911ff94b3d8df9883e1318b09e6819c64
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-26 00:43:25 +1000

    formts document

--- models/item.js
+++ models/item.js
@@ -4,10 +4,10 @@ const Schema=mongoose.Schema;
 const ItemSchema= new Schema({
     name:{type:String, required:true, maxLength: 300},
     description:{type:String, required:true},
-    category:{type:Schema.Types.ObjectId,ref:'Category', required:true},
+    category:{type:Schema.Types.ObjectId, ref:'Category', required:true},
     price:{type:Number,min:1},
     stock:{type:Number},
-    brand:{type:Schema.Types.ObjectId,ref:'Brand',required:true}
+    brand:{type:Schema.Types.ObjectId, ref:'Brand',required:true}
 });
 
 ItemSchema.virtual('url').get(function(){
