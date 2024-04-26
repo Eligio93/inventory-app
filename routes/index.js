1c2e7d8fb564c12da7ceda8632ec5d2db69ba3d5
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-26 11:49:14 +1000

    init route of all items

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+//controller modules
+const itemController=require('../controllers/itemController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/*items list*/
+router.get('/items',itemController.items_list)
+
 
 module.exports = router;
