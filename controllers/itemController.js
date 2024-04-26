1185f55ca005f30182aea832a4bedc1ff7977c05
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-26 16:06:40 +1000

    adds controller to display items list

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -4,8 +4,7 @@ const asyncHandler= require('express-async-handler')
 
 /*display all items list*/
 exports.items_list=asyncHandler(async(req,res,next)=>{
-    const allItems= await Item.find({}).sort({name:1}).exec();
-    console.log(allItems)
+    const allItems= await Item.find({},' name description').sort({name:1}).exec();
 
     res.render('items_list',{title:'Items List', items_list: allItems})
 })
