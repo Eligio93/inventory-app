8cf75d9dc5285704ff8a62b4baddf2b6d9c3ccab
Eligio93 <eligio.cristantielli@gmail.com>
2024-04-30 15:17:13 +1000

    adds routes for edit category and brands

--- routes/index.js
+++ routes/index.js
@@ -1,68 +1,95 @@
 const express = require('express');
 const router = express.Router();
 //controller modules
-const itemController=require('../controllers/itemController')
+const itemController = require('../controllers/itemController')
 const categoryController = require('../controllers/categoryController')
 const brandController = require('../controllers/brandController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/// ITEMS ROUTES ///
+
 /*GET items list*/
-router.get('/items',itemController.items_list)
+router.get('/items', itemController.items_list)
 
 /*GET item form to create*/
-router.get('/items/create',itemController.item_create);
+router.get('/items/create', itemController.item_create);
 
 /*POST item create form*/
-router.post('/items/create',itemController.item_create_post)
+router.post('/items/create', itemController.item_create_post)
 
 /*GET Item detail*/
-router.get('/items/:id',itemController.item_detail);
+router.get('/items/:id', itemController.item_detail);
 
 /*GET Item delete*/
-router.get('/items/:id/delete',itemController.item_delete_get);
+router.get('/items/:id/delete', itemController.item_delete_get);
 
 /*POST item delete*/
-router.post('/items/:id/delete',itemController.item_delete_post)
+router.post('/items/:id/delete', itemController.item_delete_post)
 
 /*GET form for UPDATE Item*/
-router.get('/items/:id/edit',itemController.item_edit_get)
+router.get('/items/:id/edit', itemController.item_edit_get)
 
 /*POST form to UPDATE Item*/
-router.post('/items/:id/edit',itemController.item_edit_post)
+router.post('/items/:id/edit', itemController.item_edit_post)
+
+
+/// CATEGORIES ROUTES ///
 
 /*GET categories list*/
-router.get('/categories',categoryController.categories_list)
+router.get('/categories', categoryController.categories_list)
 
 /*GET category form*/
-router.get('/categories/create',categoryController.category_form_get)
+router.get('/categories/create', categoryController.category_form_get)
 
 /*POST new category form*/
-router.post('/categories/create',categoryController.category_form_post)
+router.post('/categories/create', categoryController.category_form_post)
 
 /*GET category detail*/
-router.get('/categories/:id',categoryController.category_detail)
+router.get('/categories/:id', categoryController.category_detail)
 
 /*GET category delete*/
-router.get('/categories/:id/delete',categoryController.category_delete_get)
+router.get('/categories/:id/delete', categoryController.category_delete_get)
 
 /*POST category delete*/
-router.post('/categories/:id/delete',categoryController.category_delete_post)
+router.post('/categories/:id/delete', categoryController.category_delete_post)
+
+/*GET form for UPDATE category*/
+router.get('/categories/:id/edit', categoryController.category_edit_get)
+
+/*POST form to UPDATE category*/
+router.post('/categories/:id/edit', categoryController.category_edit_post)
+
+
+///BRANDS ROUTES///
 
 /*GET brands list*/
-router.get('/brands',brandController.brands_list)
+router.get('/brands', brandController.brands_list)
 
 /*GET brand create*/
-router.get('/brands/create',brandController.brand_create_get)
+router.get('/brands/create', brandController.brand_create_get)
 
 /*POST brand create*/
-router.post('/brands/create',brandController.brand_create_post)
+router.post('/brands/create', brandController.brand_create_post)
 
 /*GET brand detail*/
-router.get('/brands/:id',brandController.brand_detail)
+router.get('/brands/:id', brandController.brand_detail)
+
+/*GET brand delete*/
+router.get('/brands/:id/delete', brandController.brand_delete_get)
+
+/*POST brand delete*/
+router.post('/brands/:id/delete', brandController.brand_delete_post)
+
+/*GET form for UPDATE brand*/
+router.get('/brands/:id/edit', brandController.brand_edit_get)
+
+/*POST form to UPDATE brand*/
+router.post('/brands/:id/edit', brandController.brand_edit_post)
 
 
 
